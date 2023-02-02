c509ea89ee2034ea182d0e39be700a8c14027496
Morgan <Mharrisstoertz@gmail.com>
2023-02-02 15:08:49 -0500

    added history view

---
+++ ShareTheBill/Views/HistoryView.swift
@@ -0,0 +1,35 @@
+//
+//  HistoryView.swift
+//  ShareTheBill
+//
+//  Created by Morgan Harris-Stoertz on 2023-02-02.
+//
+
+import SwiftUI
+
+struct HistoryView: View {
+    //MARK: stored properties
+    // Stores the history of tip calculations
+    @Binding var history: [Result]
+    
+    //MARK: computed properties
+    var body: some View {
+        VStack {
+            
+            Text("History")
+                .font(.headline.smallCaps())
+                .padding()
+            
+            List(history.reversed()) { somePriorResult in
+                ResultView(priorResult: somePriorResult)
+            }
+
+        }
+    }
+}
+
+//struct HistoryView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        HistoryView()
+//    }
+//}
